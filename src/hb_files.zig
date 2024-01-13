10004fce667ccc96155739bd5c95e5403b9d1585
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-01-13 17:48:40 -0300

    messing around

--- src/hb_files.zig
+++ src/hb_files.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
-pub fn read() !void {}
+pub const Element = struct { v: ?f64, i: usize, j: usize };
+
+pub fn read(file: []const u8) ![]const Element {
+    std.debug.print("{s}\n", .{file});
+    const elements = [_]Element{ Element{ .v = 0.1, .i = 0, .j = 0 }, Element{ .v = 0.2, .i = 1, .j = 1 } };
+
+    return &elements;
+}
 
 test "reading HB file" {}
--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,17 @@ const assert = std.debug.assert;
 const expect = std.testing.expect;
 const timer = std.time.Timer;
 
+const hb_files = @import("hb_files.zig");
+
 pub fn main() !void {
     var start = try timer.start();
 
+    const file = "input/tests/test1.mtx";
+
+    const elements = try hb_files.read(file);
+    for (elements) |e| print("{any}\n", .{e});
     // Nanosegundos para milissegundos
     // const end = start.read() / 1_000_000;
     // print("{}\n", .{end});
-    print("\n{}\n", .{std.fmt.fmtDuration(start.read())});
+    print("\nTime: {}\n", .{std.fmt.fmtDuration(start.read())});
 }
