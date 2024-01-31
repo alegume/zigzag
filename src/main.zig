c6c8bcba0ba8ad287970acb58f449cccdab5ee76
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-01-31 10:37:08 -0300

    messing around with Element and allocators

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const timer = std.time.Timer;
 
 const hb_files = @import("hb_files.zig");
 
-const size = 5_000_000_000;
+const SIZE = 900_000;
 
 pub fn main() !void {
     // var start = try timer.start();
@@ -23,43 +23,41 @@ pub fn main() !void {
     var gpa: u8 = 0;
     var page: u8 = 0;
     var c: u8 = 0;
+    const win = 1;
+    const second = 0;
+    var total_c: f64 = undefined;
+    var total_g: f64 = undefined;
+    var total_p: f64 = undefined;
 
-    for (0..10) |_| {
+    for (0..100) |_| {
         const g = try gpa_alloc();
-        const c_time = try c_alloc();
         const p = try page_allocator();
+        const c_time = try c_alloc();
         if (p < g) {
             if (c_time < p) {
-                c += 2;
-                page += 1;
+                c += win;
+                page += second;
             } else {
-                page += 2;
-                c += 1;
+                page += win;
+                c += second;
             }
         } else {
             if (c_time < g) {
-                c += 2;
-                gpa += 1;
+                c += win;
+                gpa += second;
             } else {
-                gpa += 2;
-                c += 1;
+                gpa += win;
+                c += second;
             }
         }
+        total_c += @as(f64, @floatFromInt(c_time));
+        total_p += @as(f64, @floatFromInt(p));
+        total_g += @as(f64, @floatFromInt(g));
         print("\n gpa:{} ; page:{} ; c:{} \n", .{ gpa, page, c });
+        print("\n TOTAL time \n g:{} \n p:{} \n c:{} \n", .{ total_g, total_p, total_c });
+        const percent: f64 = (total_c - total_p) / total_c * 100;
+        print("c - page: \n {d} % \n", .{percent});
     }
-}
-
-test "basics" {
-    const elements = [_]i32{ 1, 2, 3, -9, -100 };
-    for (elements, 0..) |e, i| {
-        // print("{any}\n", .{e});
-        try expect(e == elements[i]);
-    }
-
-    // // Many pointers
-    // const mp: [*]usize = [_]usize{};
-    // print("{}\n", .{mp});
-
 }
 
 fn page_allocator() !u64 {
@@ -67,20 +65,27 @@ fn page_allocator() !u64 {
 
     const allocator = std.heap.page_allocator;
     // const size = std.math.pow(usize, 2, 20);
-    const memory = try allocator.alloc(usize, size);
+    const memory = try allocator.alloc(hb_files.Element, SIZE);
     defer allocator.free(memory);
 
-    try expect(memory.len == size);
-    try expect(@TypeOf(memory) == []usize);
+    try expect(memory.len == SIZE);
+    try expect(@TypeOf(memory) == []hb_files.Element);
     try expect(@sizeOf(usize) == @sizeOf(u64));
 
     print("\n\t ** Page allocator **\n", .{});
-    // for (memory, 0..) |_, i|
-    //     memory[i] = i;
-    // for (memory, 0..) |_, i|
-    //     memory[i] *= 3;
-    memory[0] = 999;
-    memory[9999] = 21;
+    for (memory, 0..) |_, i|
+        memory[i] = hb_files.Element{
+            .v = @as(f64, @floatFromInt(i)),
+            .i = i,
+            .j = i,
+        };
+    for (memory, 0..) |_, i| {
+        memory[i].v = (memory[i].v orelse 321) * @as(f64, @floatFromInt(i));
+        memory[i].j *= 6;
+        memory[i].j *= 7;
+    }
+    memory[0] = hb_files.Element{ .v = null, .i = 435, .j = 798 };
+    memory[9999] = hb_files.Element{ .v = null, .i = 32435, .j = 87 };
     const time = start.read();
 
     print("Bench Time: {}\n", .{std.fmt.fmtDuration(time)});
@@ -101,17 +106,17 @@ fn gpa_alloc() !u64 {
         if (deinit_status == .leak) expect(false) catch @panic("TEST FAIL");
     }
 
-    const memory = try allocator.alloc(usize, size);
+    const memory = try allocator.alloc(usize, SIZE);
     defer allocator.free(memory);
 
-    try expect(memory.len == size);
+    try expect(memory.len == SIZE);
     try expect(@TypeOf(memory) == []usize);
 
     print("\n\t ** GPA allocator **\n", .{});
-    // for (memory, 0..) |_, i|
-    //     memory[i] = i;
-    // for (memory, 0..) |_, i|
-    //     memory[i] *= 3;
+    for (memory, 0..) |_, i|
+        memory[i] = i;
+    for (memory, 0..) |_, i|
+        memory[i] *= 3;
     memory[0] = 999;
     memory[9999] = 21;
     const time = start.read();
@@ -126,18 +131,18 @@ fn c_alloc() !u64 {
 
     const allocator = std.heap.c_allocator;
     // const size = std.math.pow(usize, 2, 20);
-    const memory = try allocator.alloc(usize, size);
+    const memory = try allocator.alloc(usize, SIZE);
     defer allocator.free(memory);
 
-    try expect(memory.len == size);
+    try expect(memory.len == SIZE);
     try expect(@TypeOf(memory) == []usize);
     try expect(@sizeOf(usize) == @sizeOf(u64));
 
     print("\n\t ** C allocator **\n", .{});
-    // for (memory, 0..) |_, i|
-    //     memory[i] = i;
-    // for (memory, 0..) |_, i|
-    //     memory[i] *= 3;
+    for (memory, 0..) |_, i|
+        memory[i] = i;
+    for (memory, 0..) |_, i|
+        memory[i] *= 3;
     memory[0] = 999;
     memory[9999] = 21;
     const time = start.read();
