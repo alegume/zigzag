2f3d90b3c768c05d930ce99fe96a5756c8c6fcc1
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2024-02-23 17:17:52 -0300

    Sort generic list by row

--- src/csr_matrix.zig
+++ src/csr_matrix.zig
@@ -3,7 +3,16 @@ const Matrix = @import("matrix.zig").Matrix;
 const readAsMatrix = @import("matrix.zig").readAsMatrix;
 const mm = @import("mm_files.zig");
 const EntriesType = mm.EntriesType;
-// const assert = std.debug.assert;
+const assert = std.debug.assert;
+const expect = std.testing.expect;
+
+fn Element(comptime T: type) type {
+    return struct {
+        v: ?T,
+        i: usize,
+        j: usize
+    };
+}
 
 pub fn CSR_Matrix(comptime T: type) type {
     return struct {
@@ -12,6 +21,7 @@ pub fn CSR_Matrix(comptime T: type) type {
         row_index: []usize, // Indices in v/rol_index where the rows starts
         nz_len: usize = 0,  // Non zeros elements
         m: usize = 0,       // number os rows/columns
+        entries_type: EntriesType = undefined,
 
         const Self = @This();
         pub fn init(m: usize, nz_len: usize, entries_type: EntriesType, allocator: std.mem.Allocator) Self {
@@ -34,6 +44,103 @@ pub fn CSR_Matrix(comptime T: type) type {
     };
 }
 
+pub fn csrFromFile(comptime T: type, path: []const u8, allocator:std.mem.Allocator) !CSR_Matrix(T) {
+    var file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var buf: [1024]u8 = undefined;
+    var lines_read: usize = 0;
+    var n_lines: usize = 0;
+    var m: usize = 0;
+    var n: usize = 0;
+    const csr = try allocator.create(CSR_Matrix(T));
+    var el: Element(T) = undefined;
+    var element_list = std.ArrayList(Element(T)).init(allocator);
+    defer element_list.deinit();
+    // const is_symmetric = mm.symmetry(path);
+
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        if (m == 0) {
+            // Ignore comments
+            if (std.mem.startsWith(u8, line, "%"))
+                continue;
+
+            // first line of file => (rows:m, columns:n, entries)
+            var it = std.mem.splitScalar(u8, line, ' ');
+            m = std.fmt.parseInt(usize, it.next() orelse "0", 10) catch 0;
+            if (m == 0) return mm.ReadingError.HeaderError;
+
+            // Read n_lines (third number)
+            n = std.fmt.parseInt(usize, it.next() orelse "0", 10) catch 0;
+            n_lines = std.fmt.parseInt(usize, it.next() orelse "0", 10) catch 0;
+
+            assert(m == n);
+            assert(n_lines > 0);
+
+            csr.* = CSR_Matrix(T).init(m, n_lines, EntriesType.int, allocator);
+        } else {
+            // Format => I1  J1  M(I1, J1)
+            var it = std.mem.splitScalar(u8, line, ' ');
+            // Parse Int in base 10
+            el.i = std.fmt.parseInt(usize, it.next() orelse unreachable, 10) catch unreachable;
+            el.j = std.fmt.parseInt(usize, it.next() orelse unreachable, 10) catch unreachable;
+
+            const val = it.next() orelse null;
+            // Patern Matrix (null and 1)
+            if (val == null) {
+                el.v = 1;
+                // if (try is_symmetric) csr.data[j-1][i-1] = 1;
+            } else { // Convert if not null
+                switch (@typeInfo(T)) {
+                    .Float => el.v = std.fmt.parseFloat(T, val.?) catch @panic("Not Float type!"),
+                    .Int => el.v = std.fmt.parseInt(T, val.?, 10) catch @panic("Not Int type!"),
+                    else => @panic("Error while reading value. Complex type???")
+                }
+            }
+            // Add Element to element_list
+            try element_list.append(el);
+
+            lines_read += 1;
+        }
+    }
+    assert(n_lines == lines_read);
+    csr.entries_type = try mm.entriesType(path);
+
+    // Sorting element_list
+    std.debug.print("{any}\n", .{element_list});
+    const sorted_list = try element_list.toOwnedSlice();
+    std.mem.sort(Element(T), sorted_list, {}, comptime ascByRow(T));
+    std.debug.print("{any}\n", .{sorted_list});
+    // Populate CSR 
+
+
+
+    return csr.*;
+}
+
+// Sort generic list by row
+fn ascByRow(comptime T: type) fn (void, Element(T), Element(T)) bool {
+    const impl = struct {
+        fn inner(context: void, a: Element(T), b: Element(T)) bool {
+            _ = context;
+            return a.i < b.i;
+        }
+    };
+    return impl.inner;
+}
+
+test "csrFromFile" {
+    const file = "input/tests/test1.mtx";
+    // const file = "input/tests/b1_ss.mtx";
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    _ = try csrFromFile(f64, file, allocator);
+
+}
+
+
 pub fn matrixToCSR(comptime T:type, matrix: Matrix(T), allocator: std.mem.Allocator) CSR_Matrix(T) {
     var csr_matrix = CSR_Matrix(T).init(matrix.row, matrix.nz_len, matrix.entries_type, allocator);
     var count: usize = 0;
@@ -55,34 +162,6 @@ pub fn matrixToCSR(comptime T:type, matrix: Matrix(T), allocator: std.mem.Alloca
 
     return csr_matrix;
 }
-
-
-pub fn csrFromFile(comptime T: type, path: []const u8, allocator:std.mem.Allocator) !CSR_Matrix(T) {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-    var list = std.ArrayList(T).init(allocator);
-    defer list.deinit();
-
-    try list.append('H');
-    std.debug.print("\ns: {any}\n", .{list});
-
-
-    const csr = try allocator.create(CSR_Matrix(T));
-    csr.* = CSR_Matrix(T).init(9, 9, EntriesType.int, allocator);
-    return csr.*;
-}
-
-test "csrFromFile" {
-    const file = "input/tests/test1.mtx";
-    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-    _ = try csrFromFile(u8, file, allocator);
-
-}
-
-
-
 
 // test "testing" {
 //     // const file = "input/apache2.mtx";
@@ -114,7 +193,6 @@ test "csrFromFile" {
 // }
 
 test "Testing CSR - test1.mtx" {
-    const expect = std.testing.expect;
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
@@ -133,7 +211,6 @@ test "Testing CSR - test1.mtx" {
 }
 
 test "Testing CSR - test2.mtx" {
-    const expect = std.testing.expect;
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
@@ -152,7 +229,6 @@ test "Testing CSR - test2.mtx" {
 }
 
 test "Testing CSR - b1_ss.mtx" {
-    const expect = std.testing.expect;
     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
